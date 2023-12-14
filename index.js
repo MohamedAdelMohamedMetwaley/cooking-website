c6d8154e9a1daf56944ca035533bc11506586df4
Mohamed Adel <mohamedadelzmd@gmail.com>
2023-12-14 16:34:05 +0200

    Create 'Browse Recipes' page and automate creating the recipe cards

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const port = 3000;
 const apiURL = "https://api.spoonacular.com/"
 const apikey = "?apiKey=7a390b2a08f54376bfb7bafc98c8c5e9";
 var randImgs = [];
+var browseImgs = [];
 
 app.use(express.static("./public/"))
 app.use(bodyParser.urlencoded({ extended: true }))
@@ -14,22 +15,29 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.get("/", (req, res) => {
     
     res.render("index.ejs", {
-      image1: randImgs[0],
-      image2: randImgs[1],
-      image3: randImgs[2],
-      image4: randImgs[3],
-      image5: randImgs[4],
-      image6: randImgs[5]
+      images: randImgs
+    })
+})
+
+app.get("/browse", async (req, res) => {    
+    res.render("./browse.ejs", {
+      browseImages: browseImgs
     })
 })
 
 // Listen method
 app.listen(port, async (req, res) => {
     try {
-        const result = await axios.get(`${apiURL}recipes/random${apikey}&number=6&tags=main course,side dish,lunch, main dish`,apikey);
+        const result = await axios.get(randImagesURL(6, "main course,side dish,lunch, main dish"))
+        const result2 = await axios.get(randImagesURL(100, ""));
         randImgs = result.data.recipes;
+        browseImgs = result2.data.recipes;
       } catch (error) {
         console.log("Error: ", JSON.stringify(error.message));
       }
     console.log("Server listening on port ", port)
 })
+
+function randImagesURL(number, tags){
+  return `${apiURL}recipes/random${apikey}&number=${number}&tags=${tags}`
+}
